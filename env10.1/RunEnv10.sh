787fffc7320ee1b06f02d2a75225a38b7fb78b63
马添翼 <matianyi@jtoushou.com>
2017-11-27 11:13:42 +0800

    add shell script

--- env10.1/RunEnv10.sh
+++ env10.1/RunEnv10.sh
@@ -6,16 +6,22 @@
 echo 'Runing 192.168.10.1 ...'
 echo 'Destroy all Containers'
 echo 'Destroy env10_2_Nginx01'
+docker stop env10_2_Nginx01
 docker rm env10_2_Nginx01
 echo 'Destroy env10_3_Redis01'
+docker stop env10_3_Redis01
 docker rm env10_3_Redis01
 echo 'Destroy env10_4_Mysql01'
+docker stop env10_4_Mysql01
 docker rm env10_4_Mysql01
 echo 'Destory env10_5_Zookeeper01'
+docker stop env10_5_Zookeeper01
 docker rm env10_5_Zookeeper01
 echo 'Destory env10_6_Mq01'
+docker stop env10_6_Mq01
 docker rm env10_6_Mq01
 echo 'Desory env10_7_Mycat01'
+docker rm env10_7_Mycat01
 docker rm env10_7_Mycat01
 echo '==================================================='
 echo 'Remove docker network bridge env10'
